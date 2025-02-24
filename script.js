f05d202eb35c305672ea18524ec42fe26dba3181
Richard Lim <s3901013@student.rmit.edu.au>
2025-02-24 15:47:59 +1100

    feat: functions to process data from api

---
+++ script.js
@@ -0,0 +1,52 @@
+const getWeatherInfo = () => {
+  fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/melbourne?unitGroup=metric&key=KR7QNXQQLPNJN5WYLDDYF2JJW&contentType=json"
+  )
+    //Turns into js object
+    .then(
+      (getObject = (response) => {
+        return response.json();
+      })
+    )
+    .then(
+      (getAllInfo = (response) => {
+        //Logs all responses
+        console.log(response);
+
+        //Logs addresses
+        getAddress = (response) => {
+          let responseAddress = response.address;
+          let responseResolvedAddress = response.resolvedAddress;
+          console.log(responseAddress);
+          console.log(responseResolvedAddress);
+          return responseAddress, responseResolvedAddress;
+        };
+
+        //Logs dates and temperatures
+        getDays = (response) => {
+          let responseDays = response.days;
+          console.log(responseDays);
+          responseDays.forEach((day) => {
+            console.log(
+              "Date: " +
+                day.datetime +
+                " Max: " +
+                day.tempmax +
+                " Min: " +
+                day.tempmin
+            );
+          });
+          return responseDays;
+        };
+        getDays(response);
+        getAddress(response);
+      })
+    )
+    .catch(
+      (catchError = (err) => {
+        console.log("error");
+      })
+    );
+};
+
+getWeatherInfo();
