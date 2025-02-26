bcfe847e9eb0f289bd43ba4f435e263fcc9ffdd3
Richard Lim <s3901013@student.rmit.edu.au>
2025-02-26 15:34:45 +1100

    refactor: update loading and fix airconditions clearing

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ dateHeader.innerText = "7 Day Forecast";
 const getWeatherInfo = async (location) => {
   try {
     const loading = document.getElementById("loading");
-    loading.innerText = "loading";
+    loading.style.display = "block";
+    loading.innerText = "Loading...";
 
     const response = await fetch(
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=KR7QNXQQLPNJN5WYLDDYF2JJW&contentType=json`
@@ -242,6 +243,7 @@ const getWeatherInfo = async (location) => {
       airConditionsHeader.textContent = "Air Conditions";
       const airConditionsFlexContainer = document.createElement("div");
       airConditionsFlexContainer.classList.add("air-conditions-flex-container");
+      airConditionsFlexContainer.classList.add("air-conditions-item");
       const airConditionsFlex1 = document.createElement("div");
       const airConditionsFlex2 = document.createElement("div");
       airConditionsFlexContainer.appendChild(airConditionsFlex1);
@@ -274,6 +276,9 @@ const getWeatherInfo = async (location) => {
     displayAirConditions(responseData);
   } catch (error) {
     console.error("Error fetching weather data:", error.message);
+  }
+  finally {
+    loading.style.display = "none"; // Hide loading dialog after fetching
   }
 };
 
